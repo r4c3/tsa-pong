8666b38e8e30e189fb083e775b647fd6fa1dc49d
rqwilliams9 <63271957+rqwilliams9@users.noreply.github.com>
2021-08-15 01:48:57 -0400

    MILLER BALL

--- app.py
+++ app.py
@@ -19,10 +19,23 @@ h3 = pygame.font.Font("assets/ebm.ttf", 62)
 p = pygame.font.Font("assets/ebm.ttf", 42)
 
 #sfx init
+mario_1 = pygame.mixer.Sound("assets/sfx/mario_ueh.wav")
+mario_1.set_volume(0.04)
+mario_2 = pygame.mixer.Sound("assets/sfx/mario_uhn.wav")
+mario_2.set_volume(0.04)
+mario_3 = pygame.mixer.Sound("assets/sfx/mario_wah.wav")
+mario_3.set_volume(0.04)
+mario_4 = pygame.mixer.Sound("assets/sfx/mario_yah.wav")
+mario_4.set_volume(0.04)
+mario_5 = pygame.mixer.Sound("assets/sfx/mario_yahh.wav")
+mario_5.set_volume(0.04)
+mario_6 = pygame.mixer.Sound("assets/sfx/mario_yahoo.wav")
+mario_6.set_volume(0.04)
+press_start = pygame.mixer.Sound("assets/sfx/press_start.wav")
 
 #color init
 grey = pygame.Color(22, 22, 22)
-blue = pygame.Color(0, 45, 114)
+blue = pygame.Color(64, 125, 220) #(0, 45, 114)
 red = pygame.Color(203, 44, 48)
 white = pygame.Color(245, 245, 245)
 
@@ -30,7 +43,7 @@ main_menu, game = True, False
 
 def Main_Menu():
     #music init
-    songs = ["accordion.wav", "curls.wav", "deepfriedfrenz.wav", "supervillaintheme.wav", "knishes.wav", "caniwatch.wav"]
+    songs = ["accordion.wav", "curls.wav", "deepfriedfrenz.wav", "supervillaintheme.wav", "knishes.wav", "caniwatch.wav", "crime.wav", "mariotheme.wav", "mega.wav", "peach.wav", "scottie.wav", "slider.wav", "stranger.wav", "zero.wav"]
     song_number = random.randint(0, len(songs) - 1)
     random.shuffle(songs)
     pygame.mixer.music.load("assets/music/" + songs[song_number])
@@ -78,6 +91,10 @@ def Main_Menu():
     center_text_3_rect = center_text_3.get_rect(center=(screen_width / 2, 450))
     center_text_4 = p.render("press \"m\" to toggle audio", True, white)
     center_text_4_rect = center_text_4.get_rect(center=(screen_width / 2, 490))
+    center_text_5 = p.render("triple tap \"esc\" to quit match", True, white)
+    center_text_5_rect = center_text_5.get_rect(center=(screen_width / 2, 530))
+    center_text_6 = p.render("have fun", True, white)
+    center_text_6_rect = center_text_6.get_rect(center=(screen_width / 2, 570))
 
     #main menu loop
     while 1:
@@ -93,21 +110,28 @@ def Main_Menu():
 
         #draw frame
         screen.fill(grey)
+
         screen.blit(title, title_rect)
         screen.blit(blue_logo, (150, 35))
         screen.blit(red_logo, (screen_width - 150 - 160, 35))
+
         pygame.draw.rect(screen, white, start_button)
         screen.blit(start_text, start_text_rect)
+
         screen.blit(left_text_1, left_text_1_rect)
         screen.blit(left_text_2, left_text_2_rect)
         screen.blit(left_text_3, left_text_3_rect)
+
         screen.blit(right_text_1, right_text_1_rect)
         screen.blit(right_text_2, right_text_2_rect)
         screen.blit(right_text_3, right_text_3_rect)
+
         screen.blit(center_text_1, center_text_1_rect)
         screen.blit(center_text_2, center_text_2_rect)
         screen.blit(center_text_3, center_text_3_rect)
         screen.blit(center_text_4, center_text_4_rect)
+        screen.blit(center_text_5, center_text_5_rect)
+        screen.blit(center_text_6, center_text_6_rect)
 
         #render frame
         pygame.display.flip()
@@ -115,33 +139,42 @@ def Main_Menu():
 
 def Game():
     game = True
-    pygame.mixer.music.pause()
+    pygame.mixer.music.set_volume(0.008)
 
     #score init
     left_player_score = 0
     right_player_score = 0
 
     #objects init
-    ball = pygame.Rect(screen_width / 2 - 15, screen_height / 15 - 12, 30, 30)
+    ball = pygame.image.load("assets/sprites/miller.png") #pygame.Rect(screen_width / 2 - 15, screen_height / 15 - 12, 30, 30)
+    ball = pygame.transform.scale(ball, (50, 50))
+    ball_rect = ball.get_rect()
+
 
     left_player = pygame.Rect(12, screen_height / 2 - 57, 24, 114)
     right_player = pygame.Rect(screen_width - 36, screen_height / 2 - 57, 24, 114)
 
     left_score_label = p.render("player 1 score:", True, blue)
-    left_score_label_rect = left_score_label.get_rect(center=(100, 20))
+    left_score_label_rect = left_score_label.get_rect(center=(190, 32))
     left_score_value = p.render(str(left_player_score), True, white)
-    left_score_value_rect = left_score_value.get_rect(center=(180, 20))
+    left_score_value_rect = left_score_value.get_rect(center=(342, 32))
 
     right_score_label = p.render("player 2 score:", True, red)
-    right_score_label_rect = right_score_label.get_rect(center=(screen_width - 100, 20))
+    right_score_label_rect = right_score_label.get_rect(center=(screen_width - 220, 32))
     right_score_value = p.render(str(right_player_score), True, white)
-    right_score_value_rect = right_score_value.get_rect(center=(screen_width - 180, 20))
+    right_score_value_rect = right_score_value.get_rect(center=(screen_width - 220 + 150, 32))
 
     #physics init
-    ball_velocity_x = 4
-    ball_velocity_y = 4
+    ball_velocity_x = random.choice([4, -4])
+    ball_velocity_y = random.choice([4, -4])
     left_player_velocity_y = 0
     right_player_velocity_y = 0
+
+    def resetBall():
+        global ball_velocity_x, ball_velocity_y
+        ball_rect.center = (screen_width / 2, screen_height / 2)
+        ball_velocity_x = random.choice([4, -4])
+        ball_velocity_y = random.choice([4, -4])
 
     #game loop
     while game:
@@ -185,19 +218,26 @@ def Game():
             right_player.bottom = screen_height
 
         #apply ball velocities
-        ball.x += ball_velocity_x
-        ball.y += ball_velocity_y
+        ball_rect.x += ball_velocity_x
+        ball_rect.y += ball_velocity_y
 
         #check and enforce ball collisions
-        if ball.top <= 0 or ball.bottom >= screen_height:
+        if ball_rect.top <= 0 or ball_rect.bottom >= screen_height:
             ball_velocity_y *= -1
-        if ball.left <= 0:
-            left_player_score += 1
-        if ball.right >= screen_width:
+        if ball_rect.left <= 0:
             right_player_score += 1
-        if ball.colliderect(left_player) or ball.colliderect(right_player):
+            resetBall()
+        if ball_rect.right >= screen_width:
+            left_player_score += 1
+            resetBall()
+        if ball_rect.colliderect(left_player) or ball_rect.colliderect(right_player):
+            random.choice([mario_1, mario_2, mario_3, mario_4, mario_5, mario_6]).play()
             ball_velocity_x *= -1
             ball_velocity_y += random.choice([-0.3, 0.4, 2, 0.55, 0.2, -0.1, 0])
+
+        #update score counters
+        right_score_value = p.render(str(right_player_score), True, white)
+        left_score_value = p.render(str(left_player_score), True, white)
 
         #draw frame
         screen.fill(grey)
@@ -207,12 +247,12 @@ def Game():
         screen.blit(right_score_label, right_score_label_rect)
         screen.blit(right_score_value, right_score_value_rect)
 
-        pygame.draw.ellipse(screen, white, ball)
+        #pygame.draw.ellipse(screen, white, ball)
+        screen.blit(ball, ball_rect)
         pygame.draw.rect(screen, blue, left_player)
         pygame.draw.rect(screen, red, right_player)
 
         #render frame
-        print(ball_velocity_x, ball_velocity_y)
         pygame.display.flip()
         clock.tick(tick_rate)
 
--- main_menu.py
+++
@@ -1,8 +0,0 @@
-def Main_Menu():
-    main_menu = True
-    while main_menu:
-        screen.fill(grey)
-
-
-    pygame.display.flip()
-    clock.tick(tick_rate)
